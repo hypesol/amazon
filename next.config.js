c1a3f7e02bea7a6c0e4331ecbfe123975043db55
hypesol <hypesol@gmail.com>
2025-03-09 11:33:58 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -18,12 +18,10 @@ const nextConfig = {
         pathname: "/**",
       },
     ],
+    unoptimized: true,
   },
   output: "export",  // Enables static export for GitHub Pages
   basePath: "/amazon", // Replace with your GitHub repository name
-  images: {
-    unoptimized: true, // Required for GitHub Pages compatibility
-  },
 };
 
 module.exports = nextConfig;
