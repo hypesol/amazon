d4b3dad56dfabfb73e0307c3fe10dd02d8cfee38
hypesol <hypesol@gmail.com>
2025-04-01 22:10:46 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/amazon", // Adjust for GitHub Pages
-  assetPrefix: "/amazon/", // Ensure assets load correctly
+  // basePath: "/amazon", // Adjust for GitHub Pages
+  // assetPrefix: "/amazon/", // Ensure assets load correctly
   images: {
     unoptimized: true, // Required for static export
   },
